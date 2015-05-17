a2f97035cf9dae40df5987569d8be99663d29c8b
danm <writetodan@yahoo.com>
2015-05-17 16:38:02 +0100

    @damned make TEST_HOST optionally 'local' or 'vm'

--- spec/test_host.rb
+++ spec/test_host.rb
@@ -2,32 +2,35 @@ require 'rspec'
 require_relative 'support/test_hosts/local_test_host'
 require_relative 'support/test_hosts/vm_test_host'
 
+TEST_HOSTS = {
+  'local' => LocalTestHost.new,
+  'vm' => VmTestHost.new,
+}
+DEFAULT = TEST_HOSTS['vm']
+
+def determine_test_host
+  override = ENV['TEST_HOST']
+  return DEFAULT if override.nil?
+
+  raise "unknown TEST_HOST: '#{override}'" unless TEST_HOSTS.has_key?(override)
+
+  puts "test host override: '#{override}'"
+  TEST_HOSTS[override]
+end
+
+TEST_HOST = determine_test_host
+
 shared_context :test_host do
   def test_host_name
-    test_host.name
+    TEST_HOST.name
   end
   def test_ssh_key
-    test_host.ssh_key
+    TEST_HOST.ssh_key
   end
   before(:all) {
-    test_host.before_all self
+    TEST_HOST.before_all self
   }
 
   private
 
-  def test_host
-    override = ENV['TEST_HOST']
-    if override.nil?
-      return vm_test_host
-    end
-    puts "test host override: '#{override}'"
-    return local_test_host if override == 'local'
-    raise "unknown TEST_HOST: '#{override}'"
-  end
-  def local_test_host
-    LocalTestHost.new
-  end
-  def vm_test_host
-    VmTestHost.new
-  end
 end
